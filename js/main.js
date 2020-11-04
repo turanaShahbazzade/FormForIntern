32dc4a10772c27c1a3947c718839c4e61ed568fe
NahidaM <turaneshahbazzade@gmail.com>
2020-11-04 15:35:29 +0400

    I stoped default click function of form

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+
+let m1 = document.querySelector(".m-1");
+let laptop = document.querySelector(".laptop");
+let submit = document.querySelector(".btn");
+submit.addEventListener("click", function(e){
+    e.preventDefault();
+});
+m1.addEventListener("click", function(){
+   
+});
