689df0903855e913293bd554770281afcb390198
NahidaM <turaneshahbazzade@gmail.com>
2020-11-04 21:46:02 +0400

    laptop secende laptopa aid melumatlar olan divi gostermeye calisdim lakin bug var

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
 
 
-let m1 = document.querySelector(".m-1");
+let product = document.querySelectorAll(".product");
 let laptop = document.querySelector(".laptop");
 let submit = document.querySelector(".btn");
 submit.addEventListener("click", function(e){
     e.preventDefault();
+
 });
-m1.addEventListener("click", function(){
-   
+product.forEach((item) =>{
+    item.addEventListener("click", function(){
+
+        if(item.innerHTML=="Laptops"){
+            laptop.classList.remove("d-none");
+            laptop.classList.add("d-block");
+        }
+        else{
+            laptop.classList.remove("d-block");
+            laptop.classList.add("d-none");
+        }
+    })
 });
+
+
